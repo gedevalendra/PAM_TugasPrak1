889db34fb542660e233b56a0d866e09fbc0c6139
Avardhra <gedevalendra738@gmail.com>
2026-09-09 11:30:13 +0700

    laju lagi belum ke upload

---
+++ shared/src/commonMain/kotlin/com/example/myfirstapp/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.myfirstapp
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/example/myfirstapp/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.myfirstapp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
